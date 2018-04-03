84a0c76fe998947c9093ced1ae501f4f90381758
clfhhc <clfhhc@gmail.com>
2018-04-02 22:22:05 -0700

    modified getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,7 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
+/*var getElementsByClassName = function(className
 ) {
   // your code here
   function getElementsByClassNameInHtmlCollection (htmlCollection,className) {
@@ -18,4 +18,24 @@ var getElementsByClassName = function(className
   };
   var htmlCollection=document.querySelectorAll('*');
   return getElementsByClassNameInHtmlCollection(htmlCollection,className);
+};
+*/
+
+var getElementsByClassName = function(className
+) {
+  // your code here
+  function getElementsByClassNameInHtmlCollection (htmlElement,className) {
+    var targetElements=[];
+    if (htmlElement.classList.contains(className)) {
+      targetElements.push(htmlElement);
+    }
+    if (htmlElement.childNodes.length>0) {
+      for (let i=0;i<htmlElement.childNodes.length;i++) {
+        htmlElement.childNodes[i].nodeType===1 ? targetElements.push(...getElementsByClassNameInHtmlCollection(htmlElement.childNodes[i],className)) : null;
+      }
+    }
+    return targetElements;
+  };
+  var htmlElement=document.body;
+  return getElementsByClassNameInHtmlCollection(htmlElement,className);
 };
